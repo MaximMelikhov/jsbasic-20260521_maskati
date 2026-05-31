1f28135264ae062aca8bb790b457876eda0f0063
Maxim Melikhov <melikhov-maksim@rambler.ru>
2026-05-31 22:23:58 +0300

    Задача 1 - Факториал

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,11 @@
 function factorial(n) {
-  // ваш код...
+  function factorial(n) {
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+console.log(factorial(5));
 }
