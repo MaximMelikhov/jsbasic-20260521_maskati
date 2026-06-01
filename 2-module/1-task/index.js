3d96fd69a194b0862fb0f82b91a00acae1cf5b3a
Maxim Melikhov <melikhov-maksim@rambler.ru>
2026-06-01 17:37:31 +0300

    Функция sumSalary

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,14 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  
+  for (let key in salaries) {
+    if (typeof salaries[key] === 'number' && Number.isFinite(salaries[key])) {
+      sum += salaries[key];
+    }
+  }
+  
+  return sum;
 }
+
+console.log(sumSalary(salaries)); 
+console.log(sumSalary({})); 
